c0c8db1ba362ddaed54f878b3f6311a4cd2ba450
princehabeeb <habiy3400@gmail.com>
2023-10-12 00:12:46 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,63 @@
+// console.log('hellooo');
+
+// function logSomething() {
+//     console.log('I am the first');
+// }
+
+// setTimeout(() => {
+//     console.log('I am the next');
+//     logSomething()
+// }, 3000)
+
+
+
+// setInterval(() => {
+//   console.log('Hi Dollly');
+// }, 1000);
+
+
+// function subtractNumbers(name) {
+//     console.log(name);
+// }
+
+// subtractNumbers('Prince');
+
+
+// const names1 = ['ISA'];
+// const names2 = ['YUSUF'];
+// const names3 = ['HABIB'];
+// const names4 = ['ABBAS'];
+
+// names1>=.map(() => {
+//    console.log(names1>=names4);
+// });
+
+function mas() {
+    console.log('salim');
+}
+function nas() {
+    console.log('salisu');
+}
+function fad() {
+    console.log('fahad');
+}
+// mas.nas.fad.map(() =>{
+
+setInterval(() =>{
+console.log('');
+nas();    
+},2000)
+setInterval(() =>{
+    mas()
+},6000)
+setInterval(() =>{
+    fad()
+},10000)
+
+
+// function (params) {
+    
+
+
+
+
